0c8b8f1f5dda48243566758b8527884fc0a2fc31
Sharedbyamaresh <amaresh2394@gmail.com>
2023-05-09 11:41:56 +0530

    Added sordting buble and selection sort

---
+++ 02_Algorithms/ArrayRelatedProblem.cpp
@@ -0,0 +1,103 @@
+#include "iostream"
+#include<stdio.h>
+#include<stdint.h>
+
+#include<stdlib.h>
+
+using namespace std;
+class ArraySearchAlgorithm
+{
+    public:
+    int LinearSearch(int arr[],int len_Arr, int key)
+    {
+        for (int i=0;i<len_Arr;i++)
+        {
+            if(arr[i]==key)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    int BinarySearch(int arr[], int arr_len, int key)
+    {
+        int strt_idx=0;
+        int end_idx=arr_len-1;
+        int mid_idx= int ((strt_idx + end_idx)/2);
+        while(strt_idx <= end_idx)
+        {
+            mid_idx=int ((strt_idx + end_idx)/2);
+            if(arr[mid_idx]==key)
+            {
+                return mid_idx;
+            }
+            else if(arr[mid_idx]>key)
+            {
+                end_idx=mid_idx - 1;
+            }
+            else
+            {
+                strt_idx=mid_idx + 1;
+            }
+        }
+
+        return -1;
+    }
+
+    int * SelectionSort(int arr[],int len_arr)
+    {
+        int temp=0;
+        for (int i=0;i<=len_arr-2;i++)
+        {
+            for (int j=i+1;j<=len_arr-1;j++)
+            {
+                if(arr[j] < arr[i])
+                {
+                    temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    int * BubbleSort(int arr[],int arr_len)
+    {
+        int temp=0;
+        int cnt=0;
+        while(cnt < arr_len-1)       // we have to perform iteration only N-1 time in bubble sort
+        {
+            for (int i=0;i< arr_len-cnt;i++)     // arr_len-cnt as the kargest number in array keeps pushing towards the end of the array. so last element is sorted
+            {
+                if(arr[i]>arr[i+1])
+                {
+                    temp=arr[i+1];
+                    arr[i+1]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+            cnt++;
+        }
+        return arr;
+    }
+
+};
+
+int main ()
+{
+    int arr[5]={20,10,67,78,1};
+    int *ptr;
+    ArraySearchAlgorithm Obj;
+    // cout << Obj.BinarySearch(arr,5,49);
+
+    // ptr= Obj.SelectionSort(arr,5);  // Selection sort returns an array
+    ptr= Obj.BubbleSort(arr,5);  // Selection sort returns an array
+    for (int i=0;i<5;i++)
+    {
+        cout << *ptr<<',';
+        ptr++;
+    }
+
+}
