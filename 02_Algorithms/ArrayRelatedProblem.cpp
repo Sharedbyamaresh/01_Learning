0da37689a11f38b3dcd383e0f502362525d48668
Sharedbyamaresh <amaresh2394@gmail.com>
2023-05-20 12:09:05 +0530

    Added problems on array

--- 02_Algorithms/ArrayRelatedProblem.cpp
+++ 02_Algorithms/ArrayRelatedProblem.cpp
@@ -8,6 +8,14 @@ using namespace std;
 class ArraySearchAlgorithm
 {
     public:
+    void PrintArray(int arr[],int arr_len)
+    {
+        for (int i=0;i<=arr_len -1 ; i++)
+        {
+            cout << arr[i] <<',' ;
+        }
+        cout << endl;
+    }
     int LinearSearch(int arr[],int len_Arr, int key)
     {
         for (int i=0;i<len_Arr;i++)
@@ -83,21 +91,68 @@ class ArraySearchAlgorithm
         return arr;
     }
 
+    int * InsertionSort(int arr[],int arr_len)
+    {
+        // int j=0;    // J is required even after the j forloop in order to place the element in right order
+        int flag=0;
+        for (int i=1;i <= arr_len-1;i++)
+        {
+            flag=0;
+            int current_value=arr[i];           // this current value variable is required since the array gets updated with the new values in next loops
+            for (int j=i-1; arr[j]> current_value && j>=0; j--)
+            {
+                arr[j+1]=arr[j];
+                arr[j]=current_value;
+                
+            }
+            // if(flag==1)
+            // {
+            //     arr[j]=current_value;
+
+            // }
+            
+           
+        }
+
+        return arr;
+    }
+
+    int MaxTill_I(int arr[],int arr_len, int i_limit)
+    {
+        int max_i=INT_MIN;
+        for (int i=0 ; i<=i_limit ; i++)
+        {
+            if(arr[i]>max_i)
+            {
+                max_i=arr[i];
+            }
+        }
+
+        return max_i;
+    }
+
 };
 
 int main ()
 {
-    int arr[5]={20,10,67,78,1};
+    // int arr[5]={20,10,67,78,1};
+    int arr[5]={-5,-1,-3,2,1};
+    // int arr[6]={12,45,23,51,19,8};
     int *ptr;
     ArraySearchAlgorithm Obj;
+    cout << "from Main:" << endl;
+
     // cout << Obj.BinarySearch(arr,5,49);
 
     // ptr= Obj.SelectionSort(arr,5);  // Selection sort returns an array
-    ptr= Obj.BubbleSort(arr,5);  // Selection sort returns an array
-    for (int i=0;i<5;i++)
-    {
-        cout << *ptr<<',';
-        ptr++;
-    }
+    // ptr= Obj.BubbleSort(arr,5);  // Bubble sort returns an array
+    // ptr= Obj.InsertionSort(arr,5);  // Bubble sort returns an array
+    // for (int i=0;i<5;i++)
+    // {
+    //     cout << *ptr<<',';
+    //     ptr++;
+    // }
+
+    cout<< Obj.MaxTill_I(arr,5,3);   // i th element starts from 0
 
 }
